ff3df1307c4296c9deaf4385b54276e3e2980329
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-04-11 19:29:55 +0200

    Add new test for ship is located at the bomb point

--- Tests/ship_test.js
+++ Tests/ship_test.js
@@ -12,6 +12,18 @@ describe("checkForShip", () => {
         };
 
         expect(checkForShip(player, [9,9])).to.be.false;
+
+    });
+
+    it("should correctly report a ship located at a given players coordinate", () => {
+
+        player = {
+            ships: {
+                locations : [[0,0]]
+            }
+        };
+
+        expect(checkForShip(player, [0,0])).to.be.true;
         
     });
 });
